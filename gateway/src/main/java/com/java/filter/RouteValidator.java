84905de1141f34622f966554e1b7c8a224bf2ebc
Aziz-Gnouma <aziz.gnouma2002@gmail.com>
2024-02-24 18:44:03 +0100

    Initial commit: Adding Microservices Spring PFE folder

---
+++ gateway/src/main/java/com/java/filter/RouteValidator.java
@@ -0,0 +1,24 @@
+package com.java.filter;
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+import java.util.function.Predicate;
+
+@Component
+public class RouteValidator {
+
+    public static final List<String> openApiEndpoints = List.of(
+            "/auth/AddNewUser",
+            "/auth/authenticate",
+            "/eureka",
+            "/auth/AllUsers/Admin"
+    );
+
+    public Predicate<ServerHttpRequest> isSecured =
+            request -> openApiEndpoints
+                    .stream()
+                    .noneMatch(uri -> request.getURI().getPath().contains(uri));
+
+}
